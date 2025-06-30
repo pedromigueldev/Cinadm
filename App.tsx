73f92be134d392f430fa8abe4ca9c7c0b8dbfabf
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2025-06-29 22:03:40 -0300

    chore: Building basic navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+function HomeScreen(props: NativeStackScreenProps<RootStackParamList, 'Home'>) {
+  const {navigation} = props;
+  const [back, setBack] = useState<boolean>(false);
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    if (props.route.params?.isBack) setBack(true);
+  }, [props.route.params?.isBack]);
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text>Hey</Text>
+      <Button
+        title="Profile page"
+        onPress={() =>
+          navigation.navigate('Profile', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          })
+        }
+      />
+      {back ? <Text>is back</Text> : <Text />}
     </View>
+  );
+}
+
+function ProfileScreen(props: NativeStackScreenProps<RootStackParamList, 'Profile'>) {
+  const {navigation} = props;
+  const {itemId, otherParam} = props.route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Back to home"
+        onPress={() => navigation.popTo('Home', { isBack: true })}
+      />
+    </View>
+  );
+}
+
+export type RootStackParamList = {
+  Home: { isBack?: boolean } | undefined;
+  Profile: { itemId: number; otherParam?: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        >
+        <Stack.Screen
+          name="Home"
+          options={{ title: 'Welcome' }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Profile"
+          options={{ title: 'Profile' }}
+          component={ProfileScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
